2f8d44595a8c8862e6710d1c041c15a16a76ac62
Albert Wu <38256449+CrazyAncestor@users.noreply.github.com>
2024-09-30 13:33:41 -0500

    Add files via upload

--- CM_hw5.py
+++ CM_hw5.py
@@ -42,28 +42,7 @@ def displacement(A,make_plot=False):
 
     return t,x,y
 
-plot_separately = False
-t,x1,y = displacement(0.8,plot_separately)
-t,x2,y = displacement(0.85,plot_separately)
-t,x3,y = displacement(0.95,plot_separately)
 
-y2 = []
-
-
-for i in range(len(t)):
-    beta = 1
-    omega0 =20
-    w = (omega0**2 - beta**2)**0.5
-    tau = 2*np.pi/w
-    y2.append(np.exp(2*np.log(0.95)*(t[i])/tau))
-
-plt.plot(t,x1,label = 'x(t) for A='+str(0.8))
-plt.plot(t,x2,label = 'x(t) for A='+str(0.85))
-plt.plot(t,x3,label = 'x(t) for A='+str(0.95))
-plt.plot(t,y,label = r'y(t)=$e^{-\beta t}$')
-plt.plot(t,y2,label = r'y2(t)=$e^{2log(0.95)t/ \tau}$')
-plt.xlabel('time')
-plt.ylabel('displacement')
-plt.legend(fontsize=20)
-plt.savefig('FIG_HW5_1.png')
-plt.show()
+t,x1,y = displacement(0.8,True)
+t,x2,y = displacement(0.85,True)
+t,x3,y = displacement(0.95,True)
